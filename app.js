86faca2bb623068b9407c4b8c0bfcfe560f6b5de
determinedalways <juninho_zimba-net@hotmail.com>
2022-08-19 20:02:23 -0300

    adicionando ejs

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require("express")
 const app = express()
 const mongoose = require ("mongoose")
-const userRoute= require('./public/src/router/userRoute')
-const indexRoute = require('./public/src/router/indexRoute')
-const userController = require('../Desenvolvendo-full-stack-para-curriculo-/public/src/Controllers/UserControllers')
+const userRoute= require('./src/router/userRoute')
+const indexRoute = require('./src/router/indexRoute')
+const userController = require('./src/Controllers/UserControllers')
 
 const Person = require("./models/Person")
 
 app.use(express.static(__dirname + "/public"))
 app.set('view engine','ejs')
 app.set('views')
+app.set('views',__dirname + "/src/views")
 app.use(express.json())
 app.use(indexRoute)
 app.use(userRoute)
 app.use(express.urlencoded({extended:true}))
-
-app.set('view engine','ejs')
-app.set('views',__dirname + "/src/views")
 
 app.get("/",userController.index)
 
