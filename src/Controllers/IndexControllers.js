8f68f685bb9ade158168b56b268dfe008d733390
determinedalways <102637364+determinedalways@users.noreply.github.com>
2022-08-25 22:32:02 -0300

    linkando servidores

--- src/Controllers/IndexControllers.js
+++ src/Controllers/IndexControllers.js
@@ -1,7 +1,18 @@
 const indexController = { home: (req,res) => {
 res.render("portifolio")
-}
+},
 
+biografia: (req,res) => {
+    res.render("biografia")
+},
+
+contato: (req,res) => {
+    res.render("contato")
+
+},
+curriculo: (req,res)=> {
+    res.render("curriculo")
+}
 }
 
 module.exports = indexController
--- src/router/indexRoute.js
+++ src/router/indexRoute.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const indexController = require("../Controllers/IndexControllers")
 
-router.get ('/home',indexController.home)
+router.get ('/',indexController.home)
+router.get('/biografia',indexController.biografia)
+router.get('/contato',indexController.contato)
+router.get("/curriculo",indexController.curriculo)
 
 module.exports = router
