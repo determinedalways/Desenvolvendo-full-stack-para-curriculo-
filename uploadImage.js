3f2d10c4c5ea06254d2c2ae6a5bb7d2afb6dbcd9
determinedalways <102637364+determinedalways@users.noreply.github.com>
2022-08-26 12:49:45 -0300

    editando pastas

--- uploadImage.js
+++ uploadImage.js
@@ -1,3 +1,12 @@
 const multer = require('multer')
 
-module.exports = (multer({}))
+module.exports = (multer({storage:multer.diskStorage({
+    destination: (req,file,cd) =>  {cb(null,'/public/img')},
+    filename: (req,file,cb) => {
+        cb(null,Date.now().toString + "_" + file.originalname)
+    }
+}),
+Filefilter: (req,file,cb)=> {
+    const extensaoImg = ['image/jpg'].find(formatoAceito) => formatoAceito == 
+    file.mimetype);
+}}))
